5fb4cbe323d3a6a87c7a52f00187624124d99a87
Alex Sauve <akleman1990@gmail.com>
2026-03-20 14:20:27 +0100

    Fix Firebase config to match project settings

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCHFz1dggbhHFfZKRToNgonJErT2hIv80u",
+  apiKey: "AIzaSyCHfz1dggbHhf7KRToNgonJErI2hUv8Qu0",
   authDomain: "smart-grocery-planner-702ec.firebaseapp.com",
   projectId: "smart-grocery-planner-702ec",
   storageBucket: "smart-grocery-planner-702ec.firebasestorage.app",
   messagingSenderId: "407588666878",
-  appId: "1:407588666878:web:aa801fb8f0b1aeae854af"
+  appId: "1:407588666878:web:a3507c3fc6be9bf7e854af"
 };
 
 // Initialize Firebase
